b53251b7c685f9bc48817acfa73bc3a997f8297c
qawolfsocket <socket+qawolf1@qawolf.email>
2025-09-16 11:18:11 -0700

    Update qawolf_socket_rubygem.rb

--- lib/qawolf_socket_rubygem.rb
+++ lib/qawolf_socket_rubygem.rb
@@ -1,10 +1,12 @@
+mkdir -p lib
+cat > lib/qawolf_socket_rubygem.rb <<'RUBY'
 # frozen_string_literal: true
 
-# Module name should be CamelCase version of your gem name
 module QawolfSocketRubygem
   VERSION = "0.0.1"
 
   def self.hello
-    "hello from rubygems.org/#{name.downcase}"
+    "hello from rubygems.org/qawolf-socket-rubygem"
   end
 end
+RUBY
