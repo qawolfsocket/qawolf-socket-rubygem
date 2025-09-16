1c70303689256b9737bd7cb46458b5695f6bccd1
qawolfsocket <socket+qawolf1@qawolf.email>
2025-09-16 10:46:57 -0700

    Create qawolf_socket_rubygem.rb

---
+++ lib/qawolf_socket_rubygem.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Module name should be CamelCase version of your gem name
+module QawolfSocketRubygem
+  VERSION = "0.0.1"
+
+  def self.hello
+    "hello from rubygems.org/#{name.downcase}"
+  end
+end
